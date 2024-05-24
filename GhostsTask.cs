561b4f1ba1660e9430608e24a1d1c44da69e05c1
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-24 20:31:04 +0300

    Add DoMagic() method to GhostTask class

--- GhostsTask.cs
+++ GhostsTask.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 
 namespace hashes;
@@ -7,24 +8,96 @@ public class GhostsTask :
 	IFactory<Document>, IFactory<Vector>, IFactory<Segment>, IFactory<Cat>, IFactory<Robot>, 
 	IMagic
 {
-	public void DoMagic()
+    private string type;
+    private byte[] sourceBytes;
+    private Cat cat;
+    private Robot robot;
+    private Document document;
+    private Segment segment;
+    private Vector vector;
+    public void DoMagic()
 	{
+        switch (this.type)
+        {
+            case "Cat":
+                
+                cat.Rename($"{cat.Name}Oops");
+                break;
+            case "Rob":
+                Robot.BatteryCapacity = 200;
+                break;
+            case "Seg":
+                segment.End.Add(segment.Start);
+                break;
+            case "Vec":
+                vector.Add(vector);
+                break;
+            case "Doc":
+                sourceBytes[1] = 10;
+                break;
+            default: return;
+
+        }
 	}
 
-	// Чтобы класс одновременно реализовывал интерфейсы IFactory<A> и IFactory<B> 
-	// придется воспользоваться так называемой явной реализацией интерфейса.
-	// Чтобы отличать методы создания A и B у каждого метода Create нужно явно указать, к какому интерфейсу он относится.
-	// На самом деле такое вы уже видели, когда реализовывали IEnumerable<T>.
+    // Чтобы класс одновременно реализовывал интерфейсы IFactory<A> и IFactory<B> 
+    // придется воспользоваться так называемой явной реализацией интерфейса.
+    // Чтобы отличать методы создания A и B у каждого метода Create нужно явно указать, к какому интерфейсу он относится.
+    // На самом деле такое вы уже видели, когда реализовывали IEnumerable<T>.
 
-	Vector IFactory<Vector>.Create()
+    Document IFactory<Document>.Create()
+    {
+        if (document != null)
+        {
+            return document;
+        }
+        byte[] bytes = { 1, 2, 3 };
+        sourceBytes = bytes;
+        type = "Doc";
+        return document = new Document("doc", Encoding.Default, sourceBytes);
+    }
+
+    Robot IFactory<Robot>.Create()
+    {
+        if(robot != null)
+        {
+            return robot;
+        }
+        type = "Rob";
+        robot = new Robot("01", 100.0d);
+        return robot;
+    }
+
+    Cat IFactory<Cat>.Create()
+    {
+        if(cat != null)
+        {
+            return cat;
+        }
+        type = "Cat";
+        cat = new Cat("Kitty", "Hello", DateTime.Today);
+        return cat;
+    }
+
+    Vector IFactory<Vector>.Create()
 	{
-		throw new NotImplementedException();
+        if(vector != null)
+        { 
+            return vector;
+        }
+        type = "Vec";
+        vector = new Vector(1.0d, 3.0d);
+        return vector;
 	}
 
 	Segment IFactory<Segment>.Create()
 	{
-		throw new NotImplementedException();
+        if (segment != null)
+        {
+            return segment;
+        }
+        type = "Seg";
+        segment = new Segment(new Vector(1.0d, 1.0d), new Vector(5.0d, 5.0d));
+        return segment;
 	}
-
-	// И так даллее по аналогии...
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ghost
+
+Inaccurate implementation of Equals() and GetHashCode could lead to the disappearance of key
+that were previously added to Dictionary or HashSet.
+This task illustrates such mistakes that have been made in its classes implementation.
+
+GhostTask class`s DoMagic() method was implemented to represent what can go wrong,
+when the above-mentioned mistakes are not fixed.
